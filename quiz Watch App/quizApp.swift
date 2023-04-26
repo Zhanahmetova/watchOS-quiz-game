2ff86ba26783d45ff6e963d204eb86c7cd34bdc5
z.zhanakhmetova <z.zhanakhmetova@mycar.kz>
2023-04-26 15:33:05 +0600

    Initial Commit

---
+++ quiz Watch App/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz Watch App
+//
+//  Created by Zhanel on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct quiz_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
